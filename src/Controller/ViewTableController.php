5b45d2065bebcaac3d72e5446c804799a0edfac0
Elijah Dromarsky <157527162+CanadianBias@users.noreply.github.com>
2025-03-21 00:43:23 +0000

    Add Zenstruck Foundry configuration and integrate into the project

--- src/Controller/ViewTableController.php
+++ src/Controller/ViewTableController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PatentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ViewTableController extends AbstractController
 {
     #[Route('/view/table', name: 'app_view_table')]
-    public function index(): Response
+    public function index(PatentRepository $repository): Response
     {
+
+        $patents = $repository->findAll();
+
+        dump($patents);
+
         return $this->render('view_table/index.html.twig', [
             'controller_name' => 'ViewTableController',
         ]);
